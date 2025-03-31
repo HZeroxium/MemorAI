eef0f327f2d8a4f686ce45e1c60144acc33c07bb
MUisthebest <127029369+MUisthebest@users.noreply.github.com>
2025-03-31 10:25:35 +0700

    auto identify SHA-1 & SHA-256

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,11 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+    plugins {
+        id("com.android.application") version "<latest_version>"
+        id("org.jetbrains.kotlin.android") version "<latest_version>"
+        id("com.google.firebase.appdistribution") version "<latest_version>" // Thêm dòng này
+    }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
